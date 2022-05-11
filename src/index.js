bf496acbe7379d6f638f03814229fc7d5ebde0a0
Dwumrohl <81928766+Dwumrohl@users.noreply.github.com>
2022-05-12 00:20:27 +0400

    1

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import {faAddressCard} from "@fortawesome/free-solid-svg-icons";
 import {
     BrowserRouter as Router,
     Route,
-    Link, Routes, useParams,
+    Link, Routes, useParams, Redirect,
 } from 'react-router-dom';
 
 
@@ -27,7 +27,8 @@ export const Navbar = () => (
 
                 <main>
                     <Routes>
-                        <Route path="/ppireact/:" element={<News />}>
+                        <Route path="/ppireact/">
+                            <Redirect to="/news"/>
                         </Route>
                         <Route path="/about" element={<About />}>
                         </Route>
@@ -35,9 +36,8 @@ export const Navbar = () => (
                         </Route>
                         <Route path="/news/:id" element={<New />}>
                         </Route>
-                        <Route path="/:" element={<News />}>
-                        </Route>
-                        <Route path="/" element={<News />}>
+                        <Route path="/">
+                            <Redirect to="/news"/>
                         </Route>
                         <Route path="/bookmarks" element={<Bookmarks />}>
                         </Route>
